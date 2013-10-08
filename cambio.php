33df89c6f2e7a75f6b2cdc49fb0753f2d17cd180
Skidra <mistick_inquisidor@hotmail.com>
2013-10-07 19:09:22 -0500

    modificacion de programa php "cambio"

--- cambio.php
+++ cambio.php
@@ -1,8 +1,15 @@
+<html>
+<body>
 <?php
-$pesos=10
-$casa_cambio=15.5
-echo la cantidad en peso:".$pesos;
-echo "El valor del dolar es:".cas_cambio;
-$conversion_pesos=$casa_cambio/$pesos
-echo"La cantidad de dolares convertidos a pesos es:".$conversion_pesos;
+$dolares=10;
+$tasa=15.5;
+echo "La cantidad: ".$dolares;
+echo '<br>';
+echo "El valor del dolar es: ".$tasa;
+echo '<br>';
+$conversion=$dolares*$tasa;
+echo"Son: ".$conversion," pesos";
 ?>
+
+</body>
+</html>
